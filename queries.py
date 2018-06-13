d765fe9d8f4bf43041b8a59c597d3e49910904be
jinhan <choi.jinhan@gmail.com>
2018-06-13 22:15:07 +0900

    dd

--- queries.py
+++ queries.py
@@ -967,18 +967,18 @@ def query_card_data(sess, order, index, polls, regions, parties, candidates, tim
 		# print(candidate_region)
 		# candidate_poll에 맞게 테이블 선택		
 		if candidate_poll_code == 2: # 국회의원
-			candidate_poll_openrate = sess.query(OpenProgress2.openPercent).filter(OpenProgress2.sgg==candidate_region,OpenProgress2.datatime<=time, OpenProgress2.sggCityCode!=None).scalar()
+			candidate_poll_openrate = sess.query(func.max(OpenProgress2.openPercent)).filter(OpenProgress2.sgg==candidate_region,OpenProgress2.datatime<=time, OpenProgress2.sggCityCode!=None).scalar()
 		
 		elif candidate_poll_code == 4:
-			candidate_poll_openrate = sess.query(OpenProgress4.openPercent).filter(OpenProgress4.gusigun==candidate_region,OpenProgress4.datatime<=time, OpenProgress4.sggCityCode!=None).scalar()
+			candidate_poll_openrate = sess.query(func.max(OpenProgress4.openPercent)).filter(OpenProgress4.gusigun==candidate_region,OpenProgress4.datatime<=time, OpenProgress4.sggCityCode!=None).scalar()
 
 		elif candidate_poll_code == 3:
-			candidate_poll_openrate = sess.query(OpenProgress3.openPercent).filter(OpenProgress3.sido==candidate_region, OpenProgress3.gusigun=='합계',OpenProgress3.datatime<=time).scalar()
+			candidate_poll_openrate = sess.query(func.max(OpenProgress3.openPercent)).filter(OpenProgress3.sido==candidate_region, OpenProgress3.gusigun=='합계',OpenProgress3.datatime<=time).scalar()
 
 			candidate_poll_openrate_sub = sess.query(OpenProgress3.openPercent, OpenProgress3.gusigun).filter(OpenProgress3.datatime<=time, OpenProgress3.sido==candidate_sdName, OpenProgress3.gusigun!='합계').all()
 
 		elif candidate_poll_code == 11:
-			candidate_poll_openrate = sess.query(OpenProgress11.openPercent).filter(OpenProgress11.sido==candidate_region, OpenProgress11.gusigun=='합계',OpenProgress11.datatime<=time).scalar()
+			candidate_poll_openrate = sess.query(func.max(OpenProgress11.openPercent)).filter(OpenProgress11.sido==candidate_region, OpenProgress11.gusigun=='합계',OpenProgress11.datatime<=time).scalar()
 			# print(candidate_poll_openrate)
 			candidate_poll_openrate_sub = sess.query(OpenProgress11.openPercent, OpenProgress11.gusigun).filter(OpenProgress11.datatime<=time, OpenProgress11.sido==candidate_sdName, OpenProgress11.gusigun!='합계').all()
 
