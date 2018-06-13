3a75055e079fde6c68ca01ae6ae525d709aa8bf8
jinhan <choi.jinhan@gmail.com>
2018-06-13 19:20:30 +0900

    remove 20-5

--- generator.py
+++ generator.py
@@ -102,7 +102,10 @@ def generateMeta(args):
 					continue
 				meta_cards.append(meta_card)
 			# end for
-			# meta_cards = list({v['data']['text']:v for v in meta_cards}.values())
+			try:
+				meta_cards = list({v['data']['text']:v for v in meta_cards}.values())
+			except TypeError:
+				meta_cards = meta_cards
 			meta['cards'] = meta_cards
 			
 			if deploy_mode:
